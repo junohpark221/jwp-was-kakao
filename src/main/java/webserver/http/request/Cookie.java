5452da20473b209686bbcfb75af690851d417b8c
junohpark221 <junohpark221@gmail.com>
2024-04-18 15:03:11 +0900

    refactor: Cookie 내 split 검증 절차 추가

--- src/main/java/webserver/http/request/Cookie.java
+++ src/main/java/webserver/http/request/Cookie.java
@@ -7,23 +7,27 @@ public class Cookie {
     private final Map<String, String> contents;
 
     public Cookie(Map<String, String> request) {
-        this.contents = extractCookie(request);
+        this.contents = new HashMap<>();
+        extractCookie(request);
     }
 
-    private Map<String, String> extractCookie(Map<String, String> request) {
-        Map<String, String> cookies = new HashMap<>();
+    private void extractCookie(Map<String, String> request) {
         String cookieFromHeader = request.getOrDefault("Cookie", "");
 
         if (cookieFromHeader.isEmpty()) {
-            return cookies;
+            return;
         }
 
         for (String splits : cookieFromHeader.split("; ")) {
             String[] keyValue = splits.split("=");
-            cookies.put(keyValue[0], keyValue[1]);
+            putDataIntoCookie(keyValue);
         }
+    }
 
-        return cookies;
+    private void putDataIntoCookie(String[] keyValue) {
+        if (keyValue.length == 2) {
+            contents.put(keyValue[0], keyValue[1]);
+        }
     }
 
     public String getCookieSessionId() {
